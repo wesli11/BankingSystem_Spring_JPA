98e13bfc41b35ab5e0bd21a59790aee018b2c06b
wesli11 <35699074+wesli11@users.noreply.github.com>
2025-01-25 07:34:30 +0100

    Update ClientRepository.java

--- src/main/java/com/bank/repository/ClientRepository.java
+++ src/main/java/com/bank/repository/ClientRepository.java
@@ -11,24 +11,16 @@ import com.bank.model.Movements;
 
 public interface ClientRepository {
     
-   // Consulta el saldo de una cuenta específica
     Optional<Double> findBalanceByAccount(int idAccount);
 
-    // Realiza un ingreso en una cuenta
     boolean deposit(double quantity, int idAccount);
 
-    // Obtiene los datos de una cuenta por su ID
     Account findAccountById(int idAccount);
 
-    // Realiza un retiro de una cuenta
     boolean withdraw(double quantity, int idAccount);
 
     void updateClient(Client client);
-    //ESTA OPERACION ES DEL SERVICIO
-    // Realiza una transferencia entre cuentas
-  //  boolean transfer(int idAccountStart, int idAccountEnd, double quantity);
-
-    // Consulta los movimientos de una cuenta en un rango de fechas
+   
     List<Movements> findMovementsInDateRange(int idAccount, LocalDateTime from, LocalDateTime to);
 
    
